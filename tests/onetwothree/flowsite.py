a8fb28887e259043ab5383b89bdcd4a9bb47f67c
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2014-02-27 15:36:55 +0700

    Drop flowsite

--- tests/onetwothree/flowsite.py
+++
@@ -1,6 +0,0 @@
-from viewflow import flowsite
-from onetwothree import models
-from onetwothree.flows import v1
-
-
-flowsite.register(models.StepProcess, v1.StepFlow)
--- tests/shipment/flowsite.py
+++
@@ -1,4 +0,0 @@
-from viewflow import flowsite
-from shipment import models, flows
-
-flowsite.register(models.ShipmentProcess, flows.ShipmentFlow)
