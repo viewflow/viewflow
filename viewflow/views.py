05676f5db54d9bf790ad3194d8c8078ab8527f56
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2014-03-02 13:25:33 +0700

    Task view prototype

--- viewflow/views.py
+++ viewflow/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.forms.models import modelform_factory
+from django.shortcuts import redirect
 
-from viewflow.models import Process
+from viewflow.models import Process, Activation
 from viewflow.shortcuts import get_page
 
 
@@ -21,3 +23,21 @@ def start(request, start_task):
 
 def end(request, end_task, activation_id):
     pass
+
+
+def task(request, flow_task, activation_id):
+    activation = Activation.objects.start(activation_id, request.POST or None)
+    form_cls = modelform_factory(flow_task.flow_cls.process_model)
+    form = form_cls(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        activation.done()
+    return redirect(activation.guess_next())
+
+    templates = ('{}/flow/index.html'.format(flow_task.flow_cls._meta.app_label),
+                 'viewflow/flow/index.html')
+
+    return render(request, templates,
+                  {'form': form,
+                   'activation': activation})
