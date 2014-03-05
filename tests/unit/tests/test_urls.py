8e029f17283e2167c29c87bb480eba81a2d2dbd3
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2014-03-05 09:13:29 +0700

    Extend test cases

--- tests/unit/tests/test_urls.py
+++ tests/unit/tests/test_urls.py
@@ -1,5 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from viewflow.models import Process, Task
 from unit.flows import SingleTaskFlow
+
+urlpatterns = patterns('',  # NOQA
+    url(r'^flow/', include(SingleTaskFlow.instance.urls)))
 
 
 class TestURLPatterns(TestCase):
@@ -15,8 +21,31 @@ class TestURLPatterns(TestCase):
         self.assertEqual(SingleTaskFlow._meta.namespace, namespace)
 
 
-# Test reverse url
+class TestURLReverse(TestCase):
+    urls = 'unit.tests.test_urls'
+
+    def test_django_reverse_flow_urls_succeed(self):
+        reverse('viewflow:index', current_app=SingleTaskFlow._meta.app_label)
+        reverse('viewflow:task', args=[1], current_app=SingleTaskFlow._meta.app_label)
+        reverse('viewflow:end', args=[1], current_app=SingleTaskFlow._meta.app_label)
+
+    def test_flow_reverse_urls_succeed(self):
+        SingleTaskFlow.instance.reverse(
+            Task(process=Process(flow_cls=SingleTaskFlow),
+                 flow_task=SingleTaskFlow.start))
+        SingleTaskFlow.instance.reverse(
+            Task(process=Process(flow_cls=SingleTaskFlow),
+                 flow_task=SingleTaskFlow.task,
+                 pk=1))
+        SingleTaskFlow.instance.reverse(
+            Task(process=Process(flow_cls=SingleTaskFlow),
+                 flow_task=SingleTaskFlow.end,
+                 pk=1))
+
+    def test_get_task_absolute_url_succeed(self):
+        task = Task(process=Process(flow_cls=SingleTaskFlow),
+                    flow_task=SingleTaskFlow.task,
+                    pk=1)
+        task.get_absolute_url()
 
 # Test flowurl tag
-
-# Test task.get_absolute_url
