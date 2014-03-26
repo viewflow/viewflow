6f1d8fc7fd1870ebb81ee10d83c1ef2a39b1042f
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2014-03-26 12:51:24 +0700

    User switch and task accept urls

---
+++ tests/examples/onetwothree/migrations/0001_initial.py
@@ -0,0 +1,11 @@
+# encoding: utf8
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+    ]
---
+++ tests/examples/website.py
@@ -0,0 +1,31 @@
+from django.conf.urls import patterns, url
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+
+
+def users(request):
+    return {
+        'users': User.objects.filter(is_active=True).order_by('-username')
+    }
+
+
+def login_as(request):
+    user = request.REQUEST.get('user_pk', None)
+    if user:
+        try:
+            user = User.objects.get(pk=user)
+        except User.DoesNotExist:
+            pass
+
+    if user:
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        login(request, user)
+    else:
+        logout(request)
+
+    return redirect(request.META.get('HTTP_REFERER', '/'))
+
+
+urlpatterns = patterns('',  # NOQA
+    url(r'^login_as/$', login_as, name="login_as"))
