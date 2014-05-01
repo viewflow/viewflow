827801f1d05476ba98988916b1615cbf7453ceb4
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2014-05-01 12:29:51 +0700

    Cleanup helloworld

--- tests/examples/helloworld/flows.py
+++ tests/examples/helloworld/flows.py
@@ -1,6 +1,6 @@
 from viewflow import flow
 from viewflow.base import this, Flow
-from viewflow.views import ProcessView
+from viewflow.views import StartView, ProcessView
 from viewflow.lock import select_for_update_lock
 
 from examples.helloworld.models import HelloWorldProcess
@@ -11,16 +11,17 @@ class HelloWorldFlow(Flow):
     process_cls = HelloWorldProcess
     lock_impl = select_for_update_lock
 
-    start = flow.Start() \
-        .Activate(this.hello_request) \
-        .Available(username='helloworld/employee')
-
-    hello_request = flow.View(ProcessView, fields=['text']) \
-        .Next(this.approve)
+    start = flow.Start(StartView, fields=['text']) \
+        .Permission('helloworld.can_start_process') \
+        .Activate(this.approve)
 
     approve = flow.View(ProcessView, fields=['approved']) \
         .Permission('helloworld.can_approve_request') \
         .Next(this.send)
+
+    check_approve = flow.If(cond=lambda p: p.approved) \
+        .OnTrue(this.send) \
+        .OnFalse(this.end)
 
     send = flow.Job(send_hello_world_request) \
         .Next(this.end)
--- tests/examples/helloworld/models.py
+++ tests/examples/helloworld/models.py
@@ -5,3 +5,9 @@ from viewflow.models import Process
 class HelloWorldProcess(Process):
     text = models.CharField(max_length=50)
     approved = models.BooleanField(default=False)
+
+    class Meta:
+        permissions = [
+            ('can_start_request', 'Can start hello world request'),
+            ('can_approve_request', 'Can approve hello world request')
+        ]
