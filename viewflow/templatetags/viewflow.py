bf6aef21eb8cdff0ae6a192ba66ac7595f7097ee
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2014-06-20 19:39:13 +0700

    Base form template tags

--- viewflow/templatetags/viewflow.py
+++ viewflow/templatetags/viewflow.py
@@ -1,7 +1,10 @@
 import re
+from collections import defaultdict
+
 from django import template
 from django.apps import apps
 from django.core.urlresolvers import reverse
+from django.template.loader import get_template
 from django.template.base import Node, TemplateSyntaxError
 from django.utils.module_loading import import_by_path
 
@@ -37,6 +40,48 @@ class FlowURLNode(Node):
         return reverse(url, current_app=flow_cls._meta.namespace, kwargs=self.kwargs)
 
 
+class ViewFormNode(Node):
+    def __init__(self, template, nodelist):
+        self.template = template
+        self.nodelist = nodelist
+
+    def render(self, context):
+        template = self.template.resolve(context)
+        if not callable(getattr(template, 'render', None)):
+            # If not, we'll try get_template
+            template = get_template(template)
+
+        context['_viewparts'] = defaultdict(dict)  # part_name -> part_id -> part_nodes
+        for part in self.nodelist.get_nodes_by_type(ViewPartNode):
+            value = part.render(context)
+            part_id = part.part_id.resolve(context)
+            context['_viewparts'][part.part_name][part_id] = value
+
+        return template.render(context)
+
+
+class ViewPartNode(Node):
+    def __init__(self, part_name, part_id, nodelist):
+        self.part_name = part_name
+        self.part_id = part_id
+        self.nodelist = nodelist
+
+    def render(self, context):
+        part_id = self.part_id.resolve(context)
+
+        if part_id in context['_viewparts'][self.part_name]:
+            return context['_viewparts'][self.part_name][part_id]
+
+        for part in self.nodelist.get_nodes_by_type(ViewPartNode):
+            value = part.render(context)
+            part_id = part.part_id.resolve(context)
+
+            if part_id not in context['_viewparts'][part.part_name]:
+                context['_viewparts'][part.part_name][part_id] = value
+
+        return self.nodelist.render(context)
+
+
 @register.tag
 def flowurl(parser, token):
     """
@@ -69,3 +114,36 @@ def flowurl(parser, token):
             kwargs[name] = parser.compile_filter(value)
 
     return FlowURLNode(flow_ref, actionname, kwargs)
+
+
+@register.tag
+def viewform(parser, token):
+    """
+    Component form render
+    """
+    bits = token.split_contents()
+    if len(bits) < 2:
+        raise TemplateSyntaxError("'{}' takes at least one argument <template_name>".format(bits[0]))
+
+    template = parser.compile_filter(bits[1])
+    nodelist = parser.parse(('endviewform',))
+    parser.delete_first_token()
+
+    return ViewFormNode(template, nodelist)
+
+
+@register.tag
+def viewpart(parser, token):
+    """
+    Redefinable part of viewform template
+    """
+    bits = token.split_contents()
+    if len(bits) < 3:
+        raise TemplateSyntaxError("'{}' takes at least one argument <part_name> <part_id>".format(bits[0]))
+
+    part_name = parser.compile_filter(bits[1]).token
+    part_id = parser.compile_filter(bits[2])
+    nodelist = parser.parse(('endviewpart',))
+    parser.delete_first_token()
+
+    return ViewPartNode(part_name, part_id, nodelist)
