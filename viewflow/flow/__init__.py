494992bce43f56a463c7362d48c91a9d6cdef555
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2014-06-30 18:09:04 +0700

    Add mixins to export list. Close #50

--- viewflow/flow/__init__.py
+++ viewflow/flow/__init__.py
@@ -2,5 +2,9 @@ from viewflow.flow.base import This, Node                         # NOQA
 from viewflow.flow.end import End                                 # NOQA
 from viewflow.flow.gates import If, Switch, Split, Join, First    # NOQA
 from viewflow.flow.job import Job, flow_job                       # NOQA
-from viewflow.flow.start import Start, flow_start_view            # NOQA
-from viewflow.flow.view import View, flow_view                    # NOQA
+from viewflow.flow.start import (                                 # NOQA
+    Start, StartViewMixin, StartFormsetViewMixin,
+    StartInlinesViewMixin, StartView, flow_start_view)
+from viewflow.flow.view import (                                  # NOQA
+    View, TaskViewMixin, TaskFormViewMixin, TaskFormsetViewMixin,
+    TaskInlinesViewMixin, ProcessView, flow_view)
