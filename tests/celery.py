c62dad8d41b3e8fc869d70829c58dc2cbe76397d
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2014-10-02 05:10:46 +0000

    Fix tests celery

--- tests/celery.py
+++ tests/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
 
 app = Celery('tests')
 
