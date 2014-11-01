b545b1d6a1b0ef6ad42730a9f4855858137767e0
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2014-11-01 17:36:14 +0600

    Move viewflow.site to separate app

--- viewflow/views/__init__.py
+++ viewflow/views/__init__.py
@@ -1,3 +1,6 @@
 from .base import DetailsView                                                      # NOQA
 from .start import StartViewMixin, StartActivationViewMixin, StartProcessView      # NOQA
 from .task import TaskViewMixin, TaskActivationViewMixin, ProcessView, AssignView  # NOQA
+from .list import (AllProcessListView, AllTaskListView, AllQueueListView,          # NOQA
+                   ProcessListView, ProcessDetailView, TaskListView,
+                   QueueListView)
