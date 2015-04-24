03be39d9fd5e768c62af00b842b23b32d958746d
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2015-04-24 17:56:26 +0600

    Update reqs

--- tests/integration/models.py
+++ tests/integration/models.py
@@ -1,5 +1,9 @@
 """
 Import test models to avoid weird migrations behavour in 1.8
 """
-from .tests.test_contrib_celery import TestCeleryProcess  # NOQA
-from .tests.test_flow_gate_break import BrokenGateProcess  # NOQA
+import django
+
+if django.VERSION >= (1, 7):
+    from .tests.test_contrib_celery import TestCeleryProcess  # NOQA
+    from .tests.test_flow_gate_break import BrokenGateProcess  # NOQA
+
