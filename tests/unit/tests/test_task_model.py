13046edbe262d5fbce92f44c835725e8d7dc506f
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2015-05-17 18:52:02 +0600

    Add test

---
+++ tests/unit/tests/test_task_model.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from ..flows import FunctionFlow, DefaultProcessFunctionFlow
+
+
+class Test(TestCase):
+    def test_get_real_process(self):
+        FunctionFlow.start.run()
+        task = FunctionFlow.task_cls.objects.all()[0]
+        self.assertTrue(isinstance(task.get_process(), FunctionFlow.process_cls))
+
+    def test_get_real_process_nohop(self):
+        DefaultProcessFunctionFlow.start.run()
+        task = DefaultProcessFunctionFlow.task_cls.objects.all()[0]
+        self.assertTrue(isinstance(task.get_process(), DefaultProcessFunctionFlow.process_cls))
