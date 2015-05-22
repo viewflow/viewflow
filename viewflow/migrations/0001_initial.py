a91ec410475e3a18597225cad0d6ba905ac08e8a
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2015-05-22 11:55:35 +0600

    Fix migrations for 0.8 -> 0.9 upgrade

--- viewflow/migrations/0001_initial.py
+++ viewflow/migrations/0001_initial.py
@@ -40,7 +40,6 @@ class Migration(migrations.Migration):
                 ('token', viewflow.fields.TokenField(max_length=150, default=viewflow.token.Token('start'))),
                 ('external_task_id', models.CharField(max_length=50, null=True, blank=True, db_index=True)),
                 ('owner_permission', models.CharField(max_length=50, blank=True, null=True)),
-                ('comments', models.TextField(blank=True, null=True)),
                 ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, null=True)),
                 ('previous', models.ManyToManyField(to='viewflow.Task', related_name='leading')),
                 ('process', models.ForeignKey(to='viewflow.Process')),
