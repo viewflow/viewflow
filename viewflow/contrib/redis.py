84544f9f67d4e2584b0c0bc0f94fbc4790b2e315
Johannes Hoppe <info@johanneshoppe.com>
2016-03-02 15:26:22 +0100

    Adds custom cache suppport to the cache and redis lock
    The default cache might be cleared in other applications therefore
    the locks might get lost. This change allows to customize the
    cache backend being used for the locks.
    
    Furthermore this allows to set a separate cache backend for the
    `redis_lock` which requires that the cache backend supports
    locking.

--- viewflow/contrib/redis.py
+++ viewflow/contrib/redis.py
@@ -2,7 +2,7 @@ import time
 import random
 from contextlib import contextmanager
 
-from django.core.cache import cache
+from django.core.cache import cache as default_cache
 from django.db import transaction
 
 from viewflow.exceptions import FlowLockFailed
@@ -14,30 +14,52 @@ except ImportError:
     raise ImportError('django-redis required')
 
 
-def redis_lock(flow, attempts=5, expires=120):
+class RedisLock:
     """
-    Task lock based redis locks implemented in ``django-redis``.
+    Task lock based on redis' cache capabilities.
+
+    Example::
+
+        class MyFlow(Flow):
+            lock_impl = RedisLock(cache=caches['locks'])
+
+    The example uses a different cache. The default cache
+    is Django's ``default`` cache configuration.
+
+    ..note::
+        This lock requires a ``django-redis`` cache backend.
+
     """
 
-    @contextmanager
-    def lock(flow_cls, process_pk):
-        key = 'django-viewflow-lock-{}/{}'.format(flow_cls._meta.namespace, process_pk)
+    def __init__(self, *, cache=default_cache):
+        self.cache = cache
 
-        for i in range(attempts):
-            lock = cache.lock(key, timeout=expires)
-            stored = lock.acquire(blocking=False)
-            if stored:
-                break
-            if i != attempts - 1:
-                sleep_time = (((i + 1) * random.random()) + 2 ** i) / 2.5
-                time.sleep(sleep_time)
-        else:
-            raise FlowLockFailed('Lock failed for {}'.format(flow_cls))
+    def __call__(self, flow, attempts=5, expires=120):
+        cache = self.cache
 
-        try:
-            with transaction.atomic():
-                yield
-        finally:
-            lock.release()
+        @contextmanager
+        def lock(flow_cls, process_pk):
+            key = 'django-viewflow-lock-{}/{}'.format(flow_cls._meta.namespace, process_pk)
 
-    return lock
+            for i in range(attempts):
+                lock = cache.lock(key, timeout=expires)
+                stored = lock.acquire(blocking=False)
+                if stored:
+                    break
+                if i != attempts - 1:
+                    sleep_time = (((i + 1) * random.random()) + 2 ** i) / 2.5
+                    time.sleep(sleep_time)
+            else:
+                raise FlowLockFailed('Lock failed for {}'.format(flow_cls))
+
+            try:
+                with transaction.atomic():
+                    yield
+            finally:
+                lock.release()
+
+        return lock
+
+
+redis_lock = RedisLock()
+"""Task lock requires the default cache config to use a ``django-redis`` cache backend."""
