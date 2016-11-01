dee5eae85d16a29b9b8336fccbc247992b7436b9
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2016-11-01 18:00:22 +0600

    Extend docstrings

--- viewflow/nodes/func.py
+++ viewflow/nodes/func.py
@@ -13,24 +13,56 @@ class StartFunction(mixins.TaskDescriptionMixin,
                     mixins.CancelViewMixin,
                     mixins.PerformViewMixin,
                     Event):
+    """
+    Start function task to state a flow from the code.
+
+    Example::
+
+        class MyFlow(Flow):
+           start = (
+               flow.StartFunction(this.start_flow)
+               .Next(this.end)
+           )
+
+           ....
+
+           @flow.flow_start_func
+           def on_shipment_receive(self, activation, shipment):
+                activation.prepare()
+                activation.process.shipment = shipment
+                activation.done()
+
+    """
+
     task_type = 'START'
     activation_class = StartActivation
 
     def __init__(self, func=None, **kwargs):
+        """
+        Instantiate a StartFunction task.
+
+        :param func: Callable[activation, **kwargs]
+        """
         self.func = func if func is not None else self.start_func_default
         super(StartFunction, self).__init__(**kwargs)
 
     @method_decorator(flow_start_func)
     def start_func_default(self, activation):
+        """Default implementation for start function.
+
+        Do nothing, just create a new process instance.
+        """
         activation.prepare()
         activation.done()
         return activation
 
     def ready(self):
+        """Resolve internal `this`-referencies."""
         if isinstance(self.func, ThisObject):
             self.func = getattr(self.flow_class.instance, self.func.name)
 
     def run(self, *args, **kwargs):
+        """Execute the function task."""
         return self.func(self, *args, **kwargs)
 
 
@@ -41,22 +73,83 @@ class Function(mixins.TaskDescriptionMixin,
                mixins.CancelViewMixin,
                mixins.PerformViewMixin,
                Event):
+    """
+    Function task to be executed outside of the flow.
+
+    Example::
+
+        class MyFlow(Flow):
+            ...
+
+            shipment_received_handler = (
+               flow.Function(
+                   this.on_shipment_receive,
+                   task_loader=this.get_shipment_handler_task)
+               .Next(this.end)
+           )
+
+           ....
+
+           @flow.flow_func
+           def on_shipment_receive(self, activation, shipment):
+                activation.prepare()
+                activation.done()
+
+           def get_shipment_handler_task(self, flow_task, shipment):
+                return Task.objects.get(process=shipment.process)
+
+    Somewhere in you code::
+
+        ...
+        MyFlow.shipment_received_handler.run(shipment)
+
+    """
 
     task_type = 'FUNC'
     activation_class = FuncActivation
 
     def __init__(self, func, task_loader=None, **kwargs):
+        """
+        Instantiate a Function task.
+
+        :param func: Callable[activation, **kwargs]
+        :param task_loader: Callable[**kwargs] -> Task
+
+        `task_loader` could be a `this` reference to the class
+        instance method.
+
+        You can skip a `task_loader` if the function going to be
+        called with Task instance, ex::
+
+             class MyFlow(Flow):
+                 ...
+
+                 shipment_received_handler = (
+                     flow.Function(
+                         this.on_shipment_receive,
+                         task_loader='')
+                     .Next(this.end)
+                 )
+
+                 ...
+
+             task = Task.objects.get(pk=request.GET['task_pk'])
+             MyFlow.shipment_received_handler.run(task=task)
+
+        """
         self.func = func
         self.task_loader = task_loader
         super(Function, self).__init__(**kwargs)
 
     def ready(self):
+        """Resolve internal `this`-referencies."""
         if isinstance(self.func, ThisObject):
             self.func = getattr(self.flow_class.instance, self.func.name)
         if isinstance(self.task_loader, ThisObject):
             self.task_loader = getattr(self.flow_class.instance, self.task_loader.name)
 
     def run(self, *args, **kwargs):
+        """Execute the function task."""
         if self.task_loader is None:
             if 'task' not in kwargs:
                 if len(args) == 0 or not isinstance(args[0], self.flow_class.task_class):
--- viewflow/nodes/split.py
+++ viewflow/nodes/split.py
@@ -7,12 +7,15 @@ from ..token import Token
 
 
 class SplitActivation(AbstractGateActivation):
-    def __init__(self, **kwargs):
+    """Parallel split gateway activation."""
+
+    def __init__(self, **kwargs):   # noqa D102
         self.next_tasks = []
         super(SplitActivation, self).__init__(**kwargs)
 
     def calculate_next(self):
-        for node, cond in self.flow_task.branches:
+        """Calculate nodes list to activate."""
+        for node, cond in self.flow_task._branches:
             if cond:
                 if cond(self):
                     self.next_tasks.append(node)
@@ -24,6 +27,11 @@ class SplitActivation(AbstractGateActivation):
 
     @Activation.status.super()
     def activate_next(self):
+        """Activate next tasks for parallel execution.
+
+        Each task would have a new execution token attached,
+        the Split task token as a common prefix.
+        """
         token_source = Token.split_token_source(self.task.token, self.task.pk)
 
         for n, next_task in enumerate(self.next_tasks, 1):
@@ -36,11 +44,29 @@ class Split(mixins.TaskDescriptionMixin,
             mixins.CancelViewMixin,
             mixins.PerformViewMixin,
             Gateway):
+    """Parallel split gateway.
+
+    Activates outgoing tasks to execute concurently. Assumes that all
+    outgoing path converges at the same Join node.
+
+    If Split has no nodes to activate, FlowRuntimeError would be
+    raised.
+
+    Example::
+
+        split_clerk_warehouse = (
+            flow.Split()
+            .Next(
+                this.package_goods,
+                cond=lambda a: a.process.need_packaging)
+            .Always(this.shipment_type)
+        )
+    """
 
     task_type = 'SPLIT'
     activation_class = SplitActivation
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs):   # noqa D102
         super(Split, self).__init__(**kwargs)
         self._activate_next = []
 
@@ -55,15 +81,24 @@ class Split(mixins.TaskDescriptionMixin,
              for node, cond in self._activate_next]
 
     @property
-    def branches(self):
+    def _branches(self):
         return self._activate_next
 
     def Next(self, node, cond=None):
+        """Node to activate if condition is true.
+
+        :param cond: Callable[activation] -> bool
+
+        """
         result = copy(self)
         result._activate_next.append((node, cond))
         return result
 
     def Always(self, node):
+        """Activate that node unconditionally.
+
+        Shortcut for `.Next` without a condition
+        """
         result = copy(self)
         result._activate_next.append((node, None))
         return result
