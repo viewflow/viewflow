0f898978126800e5428bf7ac2ea69c80de9a9fc7
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2017-02-27 16:53:26 +0600

    Allow frontend views customization

--- viewflow/frontend/apps.py
+++ viewflow/frontend/apps.py
@@ -5,6 +5,7 @@ from django.conf.urls import url, include
 from django.core.urlresolvers import reverse
 from django.template import Template, TemplateDoesNotExist
 from django.template.loader import get_template
+from django.utils.module_loading import import_string
 from django.utils.module_loading import autodiscover_modules
 
 from material import frontend
@@ -18,6 +19,7 @@ class ViewflowFrontendConfig(ModuleMixin, AppConfig):
     label = 'viewflow_frontend'
     verbose_name = "Workflow"
     icon = '<i class="material-icons">assignment</i>'
+    viewset = 'viewflow.frontend.viewset.FrontendViewSet'
 
     def __init__(self, app_name, app_module):  # noqa D102
         super(ViewflowFrontendConfig, self).__init__(app_name, app_module)
@@ -40,43 +42,15 @@ class ViewflowFrontendConfig(ModuleMixin, AppConfig):
     def ready(self):
         """Import all <app>/flows.py modules."""
         autodiscover_modules('flows', register_to=self)
+        viewset_class = import_string(self.viewset)
+        self.viewset = viewset_class(self._registry)
 
     @property
     def urls(self):  # noqa D102
-        from . import views
-        from viewflow.frontend import views as frontend_views
-
         base_url = '^workflow/'
 
-        module_views = [
-            url('^$', frontend_views.AllTaskListView.as_view(ns_map=self.ns_map), name="index"),
-            url('^queue/$', frontend_views.AllQueueListView.as_view(ns_map=self.ns_map), name="queue"),
-            url('^archive/$', frontend_views.AllArchiveListView.as_view(ns_map=self.ns_map), name="archive"),
-            url('^action/unassign/$', views.TasksUnAssignView.as_view(ns_map=self.ns_map), name="unassign"),
-            url('^action/assign/$', views.TasksAssignView.as_view(ns_map=self.ns_map), name="assign"),
-        ]
-
-        app_flows = itertools.groupby(
-            self._registry.items(),
-            lambda item: item[0]._meta.app_label)
-        for app_label, items in app_flows:
-            app_views = []
-            for flow_class, flow_router in items:
-                flow_label = flow_class._meta.flow_label
-                app_views.append(
-                    url('^{}/'.format(flow_label), include(flow_router.urls, namespace=flow_label))
-                )
-
-            module_views.append(
-                url('^{}/'.format(app_label), include(app_views, namespace=app_label))
-            )
-
-        patterns = [
-            url('^', (module_views, 'viewflow', 'viewflow'))
-        ]
-
         return frontend.ModuleURLResolver(
-            base_url, patterns, module=self, app_name=self.label)
+            base_url, self.viewset.urls, module=self, app_name=self.label)
 
     def index_url(self):
         """Base view for the viewflow frontend."""
@@ -94,11 +68,7 @@ class ViewflowFrontendConfig(ModuleMixin, AppConfig):
 
     @property
     def ns_map(self):
-        """Mapping flows to registred namespaces."""
-        return {
-            flow_class: "{}:{}".format(flow_class._meta.app_label, flow_class._meta.flow_label)
-            for flow_class, flow_site in self._registry.items()
-        }
+        return self.viewset.ns_map
 
     @property
     def flows(self):
--- viewflow/frontend/viewset.py
+++ viewflow/frontend/viewset.py
@@ -1,10 +1,188 @@
+import itertools
+from django.conf.urls import url, include
+
 from ..flow.viewset import FlowViewSet as BaseFlowViewSet
-from .views import ProcessListView
+from . import views
 
 
 class FlowViewSet(BaseFlowViewSet):
     process_list_view = [
         r'^$',
-        ProcessListView.as_view(),
+        views.ProcessListView.as_view(),
         'index'
     ]
+
+
+class FrontendViewSet(object):
+    def __init__(self, registry):
+        self.registry = registry
+
+    @property
+    def ns_map(self):
+        return {
+            flow_class: "{}:{}".format(
+                flow_class._meta.app_label,
+                flow_class._meta.flow_label)
+            for flow_class, flow_site in self.registry.items()
+        }
+
+    def filter_kwargs(self, view_class, **kwargs):
+        """Add defaults and filter kwargs to only thouse that view can accept.
+
+        Viewset pass to the view only kwars that have non DEFAULT values,
+        and if the view have a corresponding attribute.
+
+        In addition, if view has `viewset` attribute, it will receive
+        the `self` instance
+
+        """
+        result = {
+            'ns_map': self.ns_map,
+            'viewset': self,
+        }
+        result.update(kwargs)
+        return {name: value for name, value in result.items()
+                if hasattr(view_class, name)}
+
+    """
+    Inbox
+    """
+    inbox_view_class = views.AllTaskListView
+
+    def get_inbox_view(self):
+        return self.inbox_view_class.as_view(**self.get_inbox_view_kwargs())
+
+    def get_inbox_view_kwargs(self, **kwargs):
+        return self.filter_kwargs(self.inbox_view_class, **kwargs)
+
+    @property
+    def inbox_view(self):
+        return [
+            '^$',
+            self.get_inbox_view(),
+            'index'
+        ]
+
+    """
+    Queue
+    """
+    queue_view_class = views.AllQueueListView
+
+    def get_queue_view(self):
+        return self.queue_view_class.as_view(**self.get_queue_view_kwargs())
+
+    def get_queue_view_kwargs(self, **kwargs):
+        return self.filter_kwargs(self.queue_view_class, **kwargs)
+
+    @property
+    def queue_view(self):
+        return [
+            '^queue/$',
+            self.get_queue_view(),
+            "queue"
+        ]
+
+    """
+    Archive
+    """
+    archive_view_class = views.AllArchiveListView
+
+    def get_archive_view(self):
+        return self.archive_view_class.as_view(**self.get_archive_view_kwargs())
+
+    def get_archive_view_kwargs(self, **kwargs):
+        return self.filter_kwargs(self.archive_view_class, **kwargs)
+
+    @property
+    def archive_view(self):
+        return [
+            '^archive/$',
+            self.get_archive_view(),
+            "archive"
+        ]
+
+    """
+    Tasks Action: Assign
+    """
+    tasks_assign_view_class = views.TasksAssignView
+
+    def get_tasks_assign_view(self):
+        return self.tasks_assign_view_class.as_view(
+            **self.get_tasks_assign_view_kwargs())
+
+    def get_tasks_assign_view_kwargs(self, **kwargs):
+        return self.filter_kwargs(self.tasks_assign_view_class, **kwargs)
+
+    @property
+    def tasks_assign_view(self):
+        return [
+            '^action/assign/$',
+            self.get_tasks_assign_view(),
+            'assign'
+        ]
+
+    """
+    Tasks Action: Unassign
+    """
+    tasks_unassign_view_class = views.TasksUnAssignView
+
+    def get_tasks_unassign_view(self):
+        return self.tasks_unassign_view_class.as_view(
+            **self.get_tasks_unassign_view_kwargs())
+
+    def get_tasks_unassign_view_kwargs(self, **kwargs):
+        return self.filter_kwargs(self.tasks_unassign_view_class, **kwargs)
+
+    @property
+    def tasks_unassign_view(self):
+        return [
+            '^action/unassign/$',
+            self.get_tasks_unassign_view(),
+            'unassign'
+        ]
+
+    def collect_flows_urls(self):
+        result = []
+
+        app_flows = itertools.groupby(
+            self.registry.items(),
+            lambda item: item[0]._meta.app_label)
+
+        for app_label, items in app_flows:
+            app_views = []
+            for flow_class, flow_router in items:
+                flow_label = flow_class._meta.flow_label
+                app_views.append(
+                    url('^{}/'.format(flow_label), include(flow_router.urls, namespace=flow_label))
+                )
+
+            result.append(
+                url('^{}/'.format(app_label), include(app_views, namespace=app_label))
+            )
+
+        return result
+
+    def collect_viewset_urls(self):
+        result = []
+
+        url_entries = (
+            getattr(self, attr)
+            for attr in dir(self)
+            if attr.endswith('_view')
+            if isinstance(getattr(self, attr), (list, tuple))
+        )
+
+        for url_entry in url_entries:
+            url_re, view, name = url_entry
+            result.append(url(url_re, view, name=name))
+
+        return result
+
+    @property
+    def urls(self):
+        viewset_urls = self.collect_viewset_urls()
+        flow_urls = self.collect_flows_urls()
+
+        return [
+            url('^', (viewset_urls + flow_urls, 'viewflow', 'viewflow'))
+        ]
