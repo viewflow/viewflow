5e1459f57ebefec4419e18ef656c9eccaaf1ba82
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2017-05-16 11:08:47 +0600

    Update inbox/queue counts on turbolinks page render

--- viewflow/frontend/static/viewflow/js/viewflow.js
+++ viewflow/frontend/static/viewflow/js/viewflow.js
@@ -1,8 +1,16 @@
 $(document).on('turbolinks:load', function() {
   $('.dropdown-button').dropdown()
-  $('.modal').modal();
+  $('.modal').modal()
 })
 
 $(document).on('turbolinks:before-cache', function() {
-  $('.dropdown-content').css('display', 'none');
+  $('.dropdown-content').css('display', 'none')
+})
+
+$(document).on('turbolinks:before-render', function(event) {
+  $(event.originalEvent.data.newBody).find('[data-turbolinks-update]').each(function() {
+    var elem_id = $(this).attr('id'),
+        elem_value = $(this).text()
+    document.getElementById(elem_id).textContent = elem_value
+  })
 })
