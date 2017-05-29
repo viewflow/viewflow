c834bcb55d5d6e4221ead0303ef15e17993e34b6
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2017-05-30 05:21:10 +0600

    Fix badges

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='django-viewflow',
-    version='1.0.0',
+    version='1.0.1',
     author='Mikhail Podgurskiy',
     author_email='kmmbvnr@gmail.com',
     description='Reusable workflow library for django',
