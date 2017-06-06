6dda793bd6b04be50200f5bca7170d873cb845e6
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2017-06-06 17:04:06 +0600

    Fix de locale

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='django-viewflow',
-    version='1.0.1',
+    version='1.0.2',
     author='Mikhail Podgurskiy',
     author_email='kmmbvnr@gmail.com',
     description='Reusable workflow library for django',
