853aeacdcb50d15f8014410b0e6a3136c58a256e
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2019-04-30 17:05:39 +0600

    Switch to js form submit in default flow templates

--- demo/settings.py
+++ demo/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'material.frontend.middleware.SmoothNavigationMiddleware',
 )
 
 ROOT_URLCONF = 'demo.urls'
