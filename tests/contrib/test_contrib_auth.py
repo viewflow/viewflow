e2094ea15aab0698ce7e87dfb41b271e97e0a5aa
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2020-11-25 12:49:44 +0600

    Update auth templates

---
+++ tests/contrib/test_contrib_auth.py
@@ -0,0 +1,79 @@
+import html5lib
+from io import BytesIO
+from django.contrib.auth.models import User
+from django.test import TestCase, RequestFactory, tag, override_settings
+from django.urls import path
+from viewflow.contrib import auth
+
+
+@override_settings(ROOT_URLCONF=__name__)
+class Test(TestCase):
+    def setUp(self):
+        self.admin = User.objects.create_superuser('admin', 'admin@admin.com', 'admin')
+        self.user = User.objects.create_user('user', 'user@user.com', 'user')
+        self.validator = html5lib.HTMLParser(strict=True)
+
+    @tag('integration')
+    def test_change_user_avatar(self):
+        img = BytesIO(b'my_binary_data')
+        img.name = 'avatar.png'
+
+        request = RequestFactory().post('/', {'avatar': img})
+        request.user = self.admin
+
+        view = auth.ProfileView()
+        view.setup(request)
+
+        response = view.post(request)
+        self.assertEquals(response.status_code, 200)
+        self.assertIn('/media/avatars/', auth.get_user_avatar_url(self.admin))
+
+    @tag('integration')
+    def test_get_default_user_avatar_url(self):
+        """If no user avatar exists, use the default media."""
+        self.assertEqual(auth.get_user_avatar_url(self.user), '/static/viewflow/img/user.png')
+
+    def test_login_page(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+        self.validator.parse(response.content)
+
+    def test_logout_page(self):
+        response = self.client.get('/accounts/logout/')
+        self.assertEqual(response.status_code, 200)
+        self.validator.parse(response.content)
+
+    def test_password_change(self):
+        self.assertTrue(self.client.login(username='admin', password='admin'))
+        response = self.client.get('/accounts/password_change/')
+        self.assertEqual(response.status_code, 200)
+        self.validator.parse(response.content)
+
+    def test_password_change_done(self):
+        self.assertTrue(self.client.login(username='admin', password='admin'))
+        response = self.client.get('/accounts/password_change/done/')
+        self.assertEqual(response.status_code, 200)
+        self.validator.parse(response.content)
+
+    def test_password_reset_request(self):
+        response = self.client.get('/accounts/password_reset/')
+        self.assertEqual(response.status_code, 200)
+        self.validator.parse(response.content)
+
+    def test_password_reset_request_done(self):
+        response = self.client.get('/accounts/password_reset/done/')
+        self.assertEqual(response.status_code, 200)
+        self.validator.parse(response.content)
+
+    def _test_password_reset(self):
+        pass  # TODO
+
+    def test_password_reset_complete(self):
+        response = self.client.get('/accounts/reset/done/')
+        self.assertEqual(response.status_code, 200)
+        self.validator.parse(response.content)
+
+
+urlpatterns = [
+    path('accounts/', auth.AuthViewset().urls),
+]
--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,7 +1,10 @@
 import html5lib
 from django.contrib.auth.models import AnonymousUser
-from django.test import RequestFactory, TestCase, override_settings
 from django.shortcuts import render
+from django.test import RequestFactory, TestCase, override_settings
+from django.urls import path
+from django.views.generic import TemplateView
+from viewflow.urls import Application, AppMenuMixin, IndexViewMixin, Site, Viewset
 
 
 @override_settings(ROOT_URLCONF=__name__)
@@ -11,18 +14,42 @@ class Test(TestCase):
         self.request.user = AnonymousUser()
         self.validator = html5lib.HTMLParser(strict=True)
 
-    def test__base(self):
+    def test_base(self):
         response = render(self.request, 'viewflow/base.html', {})
         self.validator.parse(response.content)
 
-    def test__base_page(self):
+    def test_base__snackbar(self):
+        response = render(self.request, 'viewflow/base.html', {
+            'messages': ['Test message 1', 'Rest message 2']
+        })
+        self.assertTrue('vf-snackbar' in response.content.decode())
+        self.validator.parse(response.content)
+
+    def test_base_page(self):
         response = render(self.request, 'viewflow/base_page.html', {})
         self.validator.parse(response.content)
 
-    def test__404(self):
+    def test_base_page__with_site_menu(self):
+        response = self.client.get('/application/test/test/')
+        self.assertTrue('Test Application' in response.content.decode())
+        self.assertTrue('Test Viewset' in response.content.decode())
+        self.validator.parse(response.content)
+
+    def test_lockscreen__404(self):
         response = render(self.request, '404.html', {})
         self.validator.parse(response.content)
 
 
+class TestViewset(IndexViewMixin, AppMenuMixin, Viewset):
+    title = 'Test Viewset'
+    page_url = path('test/', TemplateView.as_view(template_name='viewflow/base_page.html'), name="page")
+
+
 urlpatterns = [
+    path('', Site(items=[
+        Application(
+            title='Test Application',
+            items=[TestViewset()]
+        )
+    ]).urls)
 ]
