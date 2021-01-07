587a85781f7786f01c74f98b5848d1b83246bcac
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2021-01-07 17:58:40 +0600

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='django-viewflow',
-    version='1.7.0',
+    version='1.8.0',
     author='Mikhail Podgurskiy',
     author_email='kmmbvnr@gmail.com',
     description='Reusable workflow library for django',
